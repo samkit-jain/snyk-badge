a2f3af083261f8127ef487e714eb32e39c588042
Samkit Jain <15127115+samkit-jain@users.noreply.github.com>
2019-08-10 18:27:10 +0530

    update readme and add comments

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# snyk-badge
-Snyk badge generator
+# snyk-badge 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Snyk badge generator for private GitHub repositories.
+
+As of August 2019, [Snyk](https://snyk.io/) badges currently only work for public npm packages and GitHub repositories, and will fail if pointed at a private repository. This Go app aims to solve that problem by providing badges for private repositories.
+
+**NOTE:** Will only work for repositories you have integrated in Snyk.
+
+## Setup
+1. Integrate Snyk with your GitHub account
+2. Install Go
+3. Set environment variables
+    ```
+    SNYK_ORG_ID="Your Snyk Organisation ID"
+    SNYK_API_KEY="Your Snyk API key"
+    ```
+4. Run `go build snyk.go && ./snyk`
+5. Visit http://localhost:8080/badge/{username}/{repo_name}/ (Replace `{username}` and `{repo_name}` with your own GitHub username and the private repository you have access to, respectively)
+
+## How it works
+Hits the [List All Projects](https://snyk.docs.apiary.io/#reference/projects/all-projects/list-all-projects) API and gets a list of all the projects in your organisation. Searches for the repo you mentioned in the URL and counts the number of issues in it. If the number of issues is 0, gives a green badge. If more than 0, gives a red badge with the total number of issues as the value. If access unavailable gives a grey badge.
+
+## Badge generation
+Badges are generated with the help of the awesome [Shields](https://github.com/badges/shields) project. Badges look like
+* <img src="https://img.shields.io/badge/vulnerabilities-0-brightgreen" alt="no vulnerabilities"/>
+* <img src="https://img.shields.io/badge/vulnerabilities-10-red" alt="10 vulnerabilities"/>
+* <img src="https://img.shields.io/badge/vulnerabilities-unknown-inactive" alt="vulnerabilities unknown"/>
--- snyk.go
+++ snyk.go
@@ -12,22 +12,32 @@ import (
 	"strings"
 )
 
+// Handle the API request for badge creation.
+// Hit Snyk's List Projects API and get a list of projects. Check if the caller has access to the repo referred to by
+// username/repo and return a badge if access.
 func badgeHandler(w http.ResponseWriter, r *http.Request, username string, repo string) {
+	// Default shields.io badge URL
 	badgeUrl := "https://img.shields.io/badge/vulnerabilities-unknown-inactive"
 
 	client := &http.Client{}
 
+	// Generate the Snyk API URL
 	apiUrl := fmt.Sprintf("https://snyk.io/api/v1/org/%s/projects", os.Getenv("SNYK_ORG_ID"))
 
+	// Setup the GET request
 	req, _ := http.NewRequest("GET", apiUrl, nil)
 
+	// Setup the headers
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", os.Getenv("SNYK_API_KEY"))
 
+	// Perform the request
 	resp, err := client.Do(req)
 
+	// Could not perform the request
+	// Likely network issues
 	if err != nil {
-		fmt.Println("Errored when sending request to the server")
+		fmt.Println("Errored when sending request to the Snyk server")
 		writeBadge(w, badgeUrl)
 		return
 	}
@@ -36,12 +46,15 @@ func badgeHandler(w http.ResponseWriter, r *http.Request, username string, repo 
 
 	respBody, _ := ioutil.ReadAll(resp.Body)
 
+	// Non-200 response received
+	// Likely the creds are wrong
 	if resp.StatusCode != 200 {
-		fmt.Println("Did not receive a 200 OK response from the server")
+		fmt.Println("Did not receive a 200 OK response from the Snyk server")
 		writeBadge(w, badgeUrl)
 		return
 	}
 
+	// Perform JSON loading of the response
 	var data map[string]interface{}
 	err = json.Unmarshal([]byte(string(respBody)), &data)
 
@@ -52,19 +65,24 @@ func badgeHandler(w http.ResponseWriter, r *http.Request, username string, repo 
 
 	projects := data["projects"].([]interface{})
 
+	// Check if the requested repo is available
 	for _, project := range projects {
 		project := project.(map[string]interface{})
 
 		if strings.HasPrefix(project["name"].(string), username+"/"+repo+":") {
+			// Count the number of issues
 			issues := project["issueCountsBySeverity"].(map[string]interface{})
 			highCount := int(issues["high"].(float64))
 			mediumCount := int(issues["medium"].(float64))
 			lowCount := int(issues["low"].(float64))
 
 			totalIssues := highCount + mediumCount + lowCount
+
 			if totalIssues == 0 {
+				// No vulnerabilities
 				badgeUrl = "https://img.shields.io/badge/vulnerabilities-0-brightgreen"
 			} else {
+				// Vulnerabilities found
 				badgeUrl = fmt.Sprintf("https://img.shields.io/badge/vulnerabilities-%d-red", totalIssues)
 			}
 
@@ -76,27 +94,35 @@ func badgeHandler(w http.ResponseWriter, r *http.Request, username string, repo 
 	return
 }
 
+// Return the badge image from the shields.io URL
 func writeBadge(w http.ResponseWriter, badgeUrl string) {
+	// Set the response header
 	w.Header().Set("Content-Type", "image/svg+xml;charset=utf-8")
+
 	client := &http.Client{}
 
 	req, _ := http.NewRequest("GET", badgeUrl, nil)
 	resp, err := client.Do(req)
 
+	// Could not perform the request
+	// Likely network issues
 	if err != nil {
-		fmt.Println("Errored when sending request to the server")
+		fmt.Println("Errored when sending request to the Shields server")
 		fmt.Fprintf(w, badgeUrl)
 		return
 	}
 
 	defer resp.Body.Close()
 
+	// Non-200 response received
+	// Likely the service is down
 	if resp.StatusCode != 200 {
-		fmt.Println("Did not receive a 200 OK response from the server")
+		fmt.Println("Did not receive a 200 OK response from the Shields server")
 		fmt.Fprintf(w, badgeUrl)
 		return
 	}
 
+	// Write the SVG image to the response object
 	io.Copy(w, resp.Body)
 }
 
@@ -104,6 +130,7 @@ var validPath = regexp.MustCompile("^/badge/([a-zA-Z0-9-]+)/([a-zA-Z0-9-]+)/$")
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Path validation
 		m := validPath.FindStringSubmatch(r.URL.Path)
 
 		if m == nil {
