8d4472b530616c2d19879d2167fda35735800816
Roberto Scudeller <robertoscudeller@Robertos-MacBook-Pro.fritz.box>
2021-12-21 13:16:31 +0100

    change code to work with azure function and update readme; remove unused files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # snyk-badge 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/b20a73f8110e4b0488a03518d0c86291)](https://www.codacy.com/app/samkit-jain/snyk-badge?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=samkit-jain/snyk-badge&amp;utm_campaign=Badge_Grade)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 Snyk badge generator for private GitHub repositories.
 
@@ -16,17 +16,55 @@ As of August 2019, [Snyk](https://snyk.io/) badges currently only work for publi
     SNYK_API_KEY="Your Snyk API key"
     ```
 4. Run `go build snyk.go && ./snyk`
-5. Visit http://localhost:8080/badge/{username}/{repo_name}/ (Replace `{username}` and `{repo_name}` with your own GitHub username and the private repository you have access to, respectively)
+5. Visit http://localhost:8080/badge/?org={username}&name={repo_name} (Replace `{username}` and `{repo_name}` with your own GitHub username and the private repository you have access to, respectively)
 
-**Note:** Trailing `/` is mandatory.
-
-**Note:** Directory `api/` is for serverless deployment on now.sh
+**Note:** You can use http://localhost:8080/badge/?org={username}&name={repo_name}&id={project_id_snyk} to be more precisely which repository you want to have a badge. Or you can sum multiple ids: http://localhost:8080/badge/?org={username}&name={repo_name}&id={project_id_snyk}&id={another_project_id_snyk}
 
 ## How it works
 Hits the [List All Projects](https://snyk.docs.apiary.io/#reference/projects/all-projects/list-all-projects) API and gets a list of all the projects in your organisation. Searches for the repo you mentioned in the URL and counts the number of issues in it. If the number of issues is 0, gives a green badge. If more than 0, gives a red badge with the total number of issues as the value. If access unavailable gives a grey badge.
 
 ## Badge generation
 Badges are generated with the help of the awesome [Shields](https://github.com/badges/shields) project. Badges look like
-* <img src="https://img.shields.io/badge/vulnerabilities-0-brightgreen" alt="no vulnerabilities"/>
-* <img src="https://img.shields.io/badge/vulnerabilities-10-red" alt="10 vulnerabilities"/>
-* <img src="https://img.shields.io/badge/vulnerabilities-unknown-inactive" alt="vulnerabilities unknown"/>
+* <img src="https://img.shields.io/badge/vulnerabilities-0-brightgreen?logo=snyk" alt="no vulnerabilities"/>
+* <img src="https://img.shields.io/badge/vulnerabilities-10-red?logo=snyk" alt="10 vulnerabilities"/>
+* <img src="https://img.shields.io/badge/vulnerabilities-unknown-inactive?logo=snyk" alt="vulnerabilities unknown"/>
+
+## Azure config and deploy
+
+Create a resource group:
+```bash
+az group create --name snykbadges-group --location eastus
+```
+Create a storage:
+```bash 
+az storage account create --name snykbadgessvc --location eastus --resource-group snykbadges-group --sku Standard_LRS
+```
+Create a function:
+```bash
+az functionapp create --name snykbadgessvc --storage-account snykbadgessvc --consumption-plan-location eastus --resource-group snykbadges-group --runtime custom --os-type Linux --functions-version 3
+```
+
+Add in functions settings in Azure Portal:
+```
+SNYK_API_KEY=asdasd...
+SNYK_ORG_ID=adsasd...
+```
+And Save it.
+
+Generate binary for linux
+```bash
+GOOS=linux GOARCH=amd64 go build snyk.go
+```
+Deploy it using func binary:
+```bash
+func azure functionapp publish snykbadgessvc
+```
+
+
+## References
+
+https://docs.microsoft.com/en-us/azure/azure-functions/create-first-function-vs-code-other  
+
+https://www.hildeberto.com/2021/01/azure-function-golang-2.html  
+
+https://acloudguru.com/blog/engineering/how-to-build-a-serverless-app-using-go-and-azure-functions  
--- snyk.go
+++ snyk.go
@@ -7,17 +7,31 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
-	"regexp"
 	"strings"
+)
+
+const (
+	// UnknownURL string
+	UnknownURL = "https://img.shields.io/badge/vulnerabilities-unknown-inactive?logo=snyk"
+	// GreenURL string
+	GreenURL = "https://img.shields.io/badge/vulnerabilities-0-brightgreen?logo=snyk"
+)
+
+var (
+	// Version string
+	Version = "1.0.0"
+	// Commit string
+	Commit = "no-data"
 )
 
 // Handle the API request for badge creation.
 // Hit Snyk's List Projects API and get a list of projects. Check if the caller has access to the repo referred to by
 // username/repo and return a badge if access.
-func badgeHandler(w http.ResponseWriter, r *http.Request, username string, repo string) {
+func badgeHandler(w http.ResponseWriter, r *http.Request, username, repo string, projectID []string) {
 	// Default shields.io badge URL
-	badgeURL := "https://img.shields.io/badge/vulnerabilities-unknown-inactive"
+	badgeURL := UnknownURL
 
 	client := &http.Client{}
 
@@ -65,33 +79,61 @@ func badgeHandler(w http.ResponseWriter, r *http.Request, username string, repo 
 
 	projects := data["projects"].([]interface{})
 
-	// Check if the requested repo is available
-	for _, project := range projects {
-		project := project.(map[string]interface{})
-
-		if strings.HasPrefix(project["name"].(string), username+"/"+repo+":") {
-			// Count the number of issues
-			issues := project["issueCountsBySeverity"].(map[string]interface{})
-			highCount := int(issues["high"].(float64))
-			mediumCount := int(issues["medium"].(float64))
-			lowCount := int(issues["low"].(float64))
-
-			totalIssues := highCount + mediumCount + lowCount
-
-			if totalIssues == 0 {
-				// No vulnerabilities
-				badgeURL = "https://img.shields.io/badge/vulnerabilities-0-brightgreen"
-			} else {
-				// Vulnerabilities found
-				badgeURL = fmt.Sprintf("https://img.shields.io/badge/vulnerabilities-%d-red", totalIssues)
-			}
-
-			break
+	// Check number of vulnerabilities based on search critiria
+	// org + name without projectID
+	// org + name + projectID
+	// org + multiple projectIDs
+	var totalIssues int
+	name := fmt.Sprintf(username + "/" + repo + ":")
+	switch len(projectID) {
+	case 0:
+		totalIssues = vulnerabilitiesFound(projects, name, "")
+	case 1:
+		totalIssues = vulnerabilitiesFound(projects, name, projectID[0])
+	default:
+		for _, id := range projectID {
+			totalIssues = vulnerabilitiesFound(projects, name, id)
 		}
+	}
+	if totalIssues == 0 {
+		// No vulnerabilities
+		badgeURL = GreenURL
+	} else {
+		// Vulnerabilities found
+		// RedURL is created here based on number of vulnerabilities found
+		badgeURL = fmt.Sprintf("https://img.shields.io/badge/vulnerabilities-%d-red?logo=snyk", totalIssues)
 	}
 
 	writeBadge(w, badgeURL)
 	return
+}
+
+func vulnerabilitiesFound(projects []interface{}, name string, projectID string) int {
+	var totalIssues int
+	for _, project := range projects {
+		project := project.(map[string]interface{})
+		if strings.HasPrefix(project["name"].(string), name) || project["id"].(string) == projectID {
+			totalIssues = countVulnerabilities(project)
+			break
+		}
+	}
+	return totalIssues
+}
+
+func countVulnerabilities(project map[string]interface{}) int {
+	var criticalCount, highCount, mediumCount, lowCount, totalIssues int
+	log.Println("project: ", project)
+	// Count the number of issues
+	issues := project["issueCountsBySeverity"].(map[string]interface{})
+	// fmt.Println("full issues list: ",issues)
+	log.Println("issues: ", issues["critical"], issues["high"], issues["medium"], issues["low"])
+	criticalCount = int(issues["critical"].(float64))
+	highCount = int(issues["high"].(float64))
+	mediumCount = int(issues["medium"].(float64))
+	lowCount = int(issues["low"].(float64))
+
+	totalIssues = criticalCount + highCount + mediumCount + lowCount
+	return totalIssues
 }
 
 // Return the badge image from the shields.io URL
@@ -126,24 +168,51 @@ func writeBadge(w http.ResponseWriter, badgeURL string) {
 	io.Copy(w, resp.Body)
 }
 
-var validPath = regexp.MustCompile("^/badge/([a-zA-Z0-9-]+)/([a-zA-Z0-9-]+)/$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Path validation
-		m := validPath.FindStringSubmatch(r.URL.Path)
-
-		if m == nil {
-			writeBadge(w, "https://img.shields.io/badge/vulnerabilities-unknown-inactive")
-			return
-		}
-
-		fn(w, r, m[1], m[2])
+func Handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("url path: ", r.URL.Path)
+	values, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		// if fails to parse URL parameters, just answer it quickly
+		writeBadge(w, UnknownURL)
+		return
 	}
+	// Parse URL parameters
+	org := values.Get("org")
+	name := values.Get("name")
+	projectID := values["id"]
+	if len(projectID) > 1 {
+		log.Println("found more than one id: ", projectID)
+	}
+	// Required values
+	// org is always required
+	// name == repository name OR
+	// projectID
+	switch {
+	case org == "":
+		writeBadge(w, UnknownURL)
+		return
+	case name == "" || len(projectID) == 0:
+		writeBadge(w, UnknownURL)
+		return
+	}
+	// try to write a correct badge
+	badgeHandler(w, r, org, name, projectID)
+}
+
+func versionHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("url path: ", r.URL.Path)
+	message := fmt.Sprintf("{\"version\": \"%s\", \"commit\":\"%s\"}", Version, Commit)
+	fmt.Fprint(w, message)
 }
 
 func main() {
-	http.HandleFunc("/badge/", makeHandler(badgeHandler))
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/api/badges", Handler)
+	http.HandleFunc("/api/version", versionHandler)
+	log.Printf("Listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
